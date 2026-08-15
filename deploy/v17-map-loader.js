8ec1abecf4704202d9d40a4d3fde59e123b0f77c
luisbg017-creator <luisbg.017@gmail.com>
2026-08-14 21:09:27 -0300

    Enable Safari v19 mobile and viewport optimization

--- deploy/v17-map-loader.js
+++ deploy/v17-map-loader.js
@@ -22,6 +22,7 @@
 
   addStylesheet('./deploy/v17-map.css?v=17', 'safari-map-v17-css');
   addStylesheet('./deploy/v18-layout.css?v=18', 'safari-layout-v18-css');
+  addStylesheet('./deploy/v19-layout.css?v=19', 'safari-layout-v19-css');
 
   const parts = [
     './deploy/v17-map-part-01.txt?v=17',
@@ -53,6 +54,7 @@
       const code = (await Promise.all(responses.map(response => response.text()))).join('');
       eval(code);
       addScript('./deploy/v18-layout.js?v=18', 'safari-layout-v18-js');
+      addScript('./deploy/v19-layout.js?v=19', 'safari-layout-v19-js');
     } catch (error) {
       console.error('No se pudo iniciar Safari Map V17:', error);
       window.__SAFARI_MAP_V17_LOADING__ = false;
